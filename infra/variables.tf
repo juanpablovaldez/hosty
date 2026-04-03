16fce6fceab902b66e5482e708f2eb2bdb93b96d
Orbitado <juanpaavaldezz@gmail.com>
2026-04-03 10:26:18 -0300

    feat: provision RDS PostgreSQL instance and update CI/CD pipeline to inject database configuration

--- infra/variables.tf
+++ infra/variables.tf
@@ -26,3 +26,21 @@ variable "ssh_public_key_path" {
   type        = string
   default     = "~/.ssh/id_rsa.pub"
 }
+
+variable "db_name" {
+  description = "Database name"
+  type        = string
+  default     = "hosty_dev"
+}
+
+variable "db_username" {
+  description = "Database master username"
+  type        = string
+  default     = "hosty"
+}
+
+variable "db_password" {
+  description = "Database master password"
+  type        = string
+  sensitive   = true
+}
