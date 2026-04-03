6a432bbdcbb2d5fc1c0b34ae1866a61474a7fbbf
Orbitado <juanpaavaldezz@gmail.com>
2026-04-03 18:09:26 -0300

    chore: enable public accessibility for RDS instance

--- infra/rds.tf
+++ infra/rds.tf
@@ -49,7 +49,7 @@ resource "aws_db_instance" "postgres" {
   db_subnet_group_name   = aws_db_subnet_group.default.name
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
 
-  publicly_accessible = false
+  publicly_accessible = true
   multi_az            = false
   storage_encrypted   = true
   skip_final_snapshot = true
